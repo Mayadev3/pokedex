a6ffc670fc548ff8a6edb418acb3ed6d098a321a
Mayadev3 <107764065+Mayadev3@users.noreply.github.com>
2022-10-09 20:23:18 +0200

    created a new element in javascript

--- script.js
+++ script.js
@@ -17,15 +17,57 @@ const colors = {
   normal: "#F5F5F5",
 };
 
+/*let animals = [
+  { name: "cat", species: "mammal", number: 10 },
+  {
+    name: "fish",
+    species: "amphibian",
+    number: 13,
+  },
+  {
+    name: "chicken",
+    species: "bird",
+    number: 20,
+  },
+];
+for (i = 0; i < animals.length; i++) {
+  if (animals[i].number > 13) {
+    console.log(animals[i].species);
+  }
+}*/
+
 async function fetchPokemons() {
   for (i = 1; i <= 150; i++) {
-    await getPokemons(i);
+    getPokemons(i);
   }
 }
+
 async function getPokemons(id) {
-  const get = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+  const get = await fetch(` https://pokeapi.co/api/v2/pokemon/${id}`);
   const response = await get.json();
   createPokemonData(response);
+  console.log(response);
 }
 fetchPokemons();
-function createPokemonData() {}
+
+function createPokemonData(pokemon) {
+  const pokemonEl = document.createElement("div");
+  pokemonEl.classList.add("pokemon");
+
+  const name = pokemon.name.toUpperCase();
+
+  const pokemonInnerHTML = `<div class="img-container">
+          <img
+            src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"" alt="${pokemon.name}"
+           
+            alt="pokemon"
+          />
+        </div>
+        <div class="info">
+          <span class="number" id="number">#001</span>
+          <p class="name">${name}</p>
+          <small class="type">Type: <span>grass</span></small>
+        </div>`;
+  pokemonEl.innerHTML = pokemonInnerHTML;
+  poke_container.appendChild(pokemonEl);
+}
